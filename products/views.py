a83fd7ff2ba543df660566d4714f9202118d2dcb
Ignas Grigaravicius <kickeris@gmail.com>
2017-07-06 15:27:17 +0100

    Product page fixed, pagination added

--- products/views.py
+++ products/views.py
@@ -3,11 +3,20 @@ from .models import Product
 from rest_framework import viewsets
 from .serializers import ProductSerializer
 from django.template.context_processors import csrf
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def all_products(request):
     products = Product.objects.all()
+    paginator = Paginator(products, 5)
+    page = request.GET.get('page')
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
     args = {}
     args.update(csrf(request))
     return render(request, "products.html", {"products": products}, args)
@@ -21,3 +30,4 @@ class ProductViewSet(viewsets.ModelViewSet):
     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
