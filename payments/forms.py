f6ea115472a9d693b00fb961666b853353f53b1e
Ignas Grigaravicius <kickeris@gmail.com>
2017-07-11 11:31:21 +0100

    Index footer fixed, search results fixed

--- payments/forms.py
+++ payments/forms.py
@@ -5,8 +5,9 @@ class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i,) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i,) for i in range(2015, 2036)]
 
+    
     credit_card_number = forms.CharField(label='Credit card number')
-    cvv = forms.CharField(label='Security code (CVV)')
     expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES)
     expiry_year = forms.ChoiceField(label="Year", choices=YEAR_CHOICES)
+    cvv = forms.CharField(label='Security code (CVV)')
     stripe_id = forms.CharField(widget=forms.HiddenInput)
